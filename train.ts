90719e6ef21d51a4dbefae840420c499772c1883
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-06-07 19:51:06 +0900

    feat: complate Task ZM

--- train.ts
+++ train.ts
@@ -1,14 +1,19 @@
-// Task ZK
-function printNumbers(): void {
-	let count = 1;
-
-	const interval = setInterval(() => {
-		console.log(count);
-		count++;
-
-		if (count > 5) {
-			clearInterval(interval);
-		}
-	}, 1000);
+// Task ZM
+function reverseInteger(input: number): number {
+	return parseInt(input.toString().split('').reverse().join(''));
 }
-printNumbers();
+console.log(reverseInteger(123456789));
+// Task ZK
+// function printNumbers(): void {
+// 	let count = 1;
+
+// 	const interval = setInterval(() => {
+// 		console.log(count);
+// 		count++;
+
+// 		if (count > 5) {
+// 			clearInterval(interval);
+// 		}
+// 	}, 1000);
+// }
+// printNumbers();
