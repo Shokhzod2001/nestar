f11a1147b8d0bca2c3fa057d20bf26979e8b2cc6
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-06-26 18:13:02 +0900

    feat: develop removeCommentByAdmin graphQL API

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -102,4 +102,13 @@ export class CommentService {
 
 		return result[0];
 	}
+
+	// ADMIN
+
+	public async removePropertyByAdmin(input: ObjectId): Promise<Comment> {
+		const result = await this.commentModel.findByIdAndDelete(input).exec();
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+
+		return result;
+	}
 }
